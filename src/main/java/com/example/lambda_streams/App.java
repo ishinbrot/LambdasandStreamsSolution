672f19fa7c147dd83354768d39cb2c015c1a2997
Ian Shinbrot <ianshinbro@gmail.com>
2021-09-14 15:49:21 -0400

    Populating methods

--- src/main/java/com/example/lambda_streams/App.java
+++ src/main/java/com/example/lambda_streams/App.java
@@ -1,8 +1,10 @@
 package com.example.lambda_streams;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class App {
 
@@ -26,13 +28,24 @@ public final class App {
     private static ArrayList<Student> determineStudentsinEachClass(ArrayList<Student> studentList, ArrayList<Professor> professorList) {
         // Populate the following method using Java streams to complete the answer
 
+        professorList.forEach(p-> {
+     //       System.out.print("Professor " + p.getFirstName() + " " + p.getLastName() + " has the following students: ");
+            Stream<Student> studentsInClass = studentList.stream().sorted(Comparator.comparing(Student::getLastName)).filter(s->s.getClassesAsString().contains(p.getClassTeaching()));
+            studentsInClass.forEach(s-> { 
+                s.addProfessor(p);
+                long grade = p.getGrade() + s.getGPA();
+                s.setGPA(grade);
+           //     System.out.println(s.getFirstName() + " " + s.getLastName() + " grade " + s.getGPA());
+            });
+     //       System.out.print("\n");
+            
+        });
         return studentList;
     }
-
+/**
+ * This function calculates each students GPA by computing their grade in the class by the amount of professors in each class
+ * @param studentList
+ * @return
+ */
     private static List<Student> computeGPA(List<Student> studentList) {
-
-
-          return studentList;
-    }
-
-}
+          studentList.forEach(s -> s.setGPA(s.getGPA() / s.getProfessorList().stream().count()));
